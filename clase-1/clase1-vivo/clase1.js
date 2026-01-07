08e9b2add48ae44a92df1b3dde9e085c33abeaaf
AndresPataFM <apfm21@Gmail.com>
2026-01-06 22:37:05 -0300

    Final clase 1

---
+++ clase-1/clase1-vivo/clase1.js
@@ -0,0 +1,82 @@
+// Variables
+
+let nombre = "Andrés"
+
+console.log("Nombre al ser declarado: ", nombre)
+
+// reasignar valor
+nombre = "Nahuel"
+
+console.log("Nombre al resasignar el valor: ",nombre)
+
+// asignaciones más complejas
+
+let numero = 10 + 7
+
+// console.log(numero)
+
+// nombre = 123 //mala práctica permitida por javascript
+
+console.log(nombre)
+
+// prompt
+
+/* let nombrePrompt = prompt("Ingrese su nombre")
+
+console.log(nombrePrompt) */
+
+//tipos de dato
+
+/* 
+4444444444455555555555564664
+4.4444444444555553e+27
+
+4444444444455555555555564663 - 4444444444455555555555564663
+0
+
+4444444444455555555555564664 - 4444444444455555555555564663
+0
+
+4444444444455555555555564664n - 4444444444455555555555564663n
+1n */
+
+// strings
+
+let frase = "hola como estás"
+
+let cita = 'Juan dijo: "Hola"'
+
+console.log(cita)
+
+let backtick = `hola`
+
+/* let nombrePrompt = prompt("Ingrese su nombre")
+
+
+let saludarSimple = "Hola " + nombrePrompt + ". ¿Cómo estás?"
+
+console.log(saludarSimple)
+
+let stringTemplate = `Hola ${nombrePrompt}. ¿Como estás? Yo estoy bien.`
+
+console.log(stringTemplate)
+
+ */
+
+/* let asdasd
+
+console.log(asdasd) */
+
+const dni = 44645645
+
+console.log(dni)
+
+// dni = 2 // error, no s puede reasignar un valro a una constante.
+
+var feo = "No debo existir"
+
+// let nombre = "Pedro" // No permite redeclarar
+
+var feo = "Soy un crimen"
+
+console.log(feo)
---
+++ clase-1/micro-1/micro1.js
@@ -0,0 +1,23 @@
+/*
+Consignas:
+🔶 Crea variables con los siguientes tipos de datos (Number, string y boolean)
+🔶 Muestra los valores de las variables en consola
+
+Tips:
+🔹 Código fuente en archivo JavaScript, con formato .js, vinculando a este con el archivo HTML mediante el tag <script>.
+🔹 Utiliza la consola para ayudarte a encontrar los errores o fallas que provocan que el código no funcione.
+*/
+
+const numero = 123;
+
+console.log("numero", typeof numero, numero)
+
+let hilo = `El número es ${numero}`
+
+// let hilo = "456456"
+
+console.log("hilo", typeof hilo, hilo)
+
+const encendido = false
+
+console.log("encendido", typeof encendido, encendido)
