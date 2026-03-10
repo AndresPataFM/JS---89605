80ca41ca726673206e35e32ebc052a06b7f96003
AndresPataFM <apfm21@Gmail.com>
2026-03-10 20:32:11 -0300

    inicio clase 9

---
+++ clase-9/clase-9-vivo/envivo9.js
@@ -0,0 +1,36 @@
+// swal
+// https://sweetalert2.github.io/
+
+const btnSwal = document.querySelector("#btnSwal")
+
+
+
+// toastify
+// https://apvarun.github.io/toastify-js/
+
+
+
+// Luxon
+// https://moment.github.io/luxon/#/?id=luxon
+
+/* const btnToast = document.getElementById("botonToastify")
+
+const DateTime = luxon.DateTime;
+
+console.log(DateTime.local(2017, 5, 15, 8, 30))
+console.log(DateTime.now().toUTC().toLocaleString(DateTime.DATETIME_MED)) */
+
+// btnToast.addEventListener("click", ()=>{
+//   Toastify({
+//         text: DateTime.now().toFormat('MMMM dd, yyyy, hh  :mm:ss'),
+//         duration: 5000,
+//         newWindow: true,
+//         gravity: "top",
+//         position: "right",
+//         stopOnFocus: true,
+//         style: {
+//           background: "linear-gradient(to right, blueviolet, blue)",
+//         }
+//       }).showToast();
+// })
+
