27a3d901ac0f13aa2b9f446624eca6faaea04cbf
AndresPataFM <apfm21@Gmail.com>
2026-03-17 20:31:42 -0300

    inicio clase 10

---
+++ clase-10/import-export/archivoImportRaiz.js
@@ -0,0 +1,13 @@
+// import { saludar, despedir, nombre } from "/clase-10/import-export/archivoExport.js";
+
+import helper from "/clase-10/import-export/archivoExport copy.js"
+
+// import sumar from "/clase-10/import-export/exportDefault.js";
+
+// console.log(helper)
+
+saludar()
+despedir()
+console.log(nombre)
+
+console.log(sumar(3,8))
